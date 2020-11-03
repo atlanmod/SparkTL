8e25f0e7504d3f7731acf299e3f725d247c013aa
Jolan Philippe <jolan.philippe@gmail.com>
2020-11-03 15:10:54 +0100

    Eval functions

--- transformation/src/main/scala/org/atlanmod/tl/sequential/Eval.scala
+++ transformation/src/main/scala/org/atlanmod/tl/sequential/Eval.scala
@@ -1,5 +1,28 @@
 package org.atlanmod.tl.sequential
 
-object Tool {
+import org.atlanmod.tl.sequential.spec.{Model, OutputPatternElement, OutputPatternElementReference, Rule, TraceLink}
+
+object Eval {
+
+    def evalGuardExpr[SME, SML, SMC, TME, TML](r: Rule[SME, SML, SMC, TME, TML], sm: Model[SME, SML], sp: List[SME])
+    : Option[Boolean] =
+        r.getGuardExpr(sm, sp)
+
+    def evalIteratorExpr[SME, SML, SMC, TME, TML](r: Rule[SME, SML, SMC, TME, TML], sm: Model[SME, SML], sp: List[SME])
+    : Int =
+        r.getIteratorExpr(sm, sp) match {
+            case Some(n) => n
+            case _ => 0
+        }
+
+    def evalOutputPatternElementExpr[SME, SML, TME, TML](sm: Model[SME, SML], sp: List[SME], iter: Int,
+                                                              o: OutputPatternElement[SME, SML, TME, TML])
+    : Option[TME] =
+        o.getElementExpr(iter, sm, sp)
+
+    def evalOutputPatternLinkExpr[SME, SML, TME, TML](sm: Model[SME, SML], sp: List[SME], oe: TME, iter: Int,
+                                  tr: List[TraceLink[SME, TME]], o: OutputPatternElementReference[SME, SML, TME, TML])
+    : Option[TML]=
+        o.getLinkExpr(tr, iter, sm, sp, oe)
 
 }
