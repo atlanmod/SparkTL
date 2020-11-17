b1d9425b5e6aa58df373fb15a7d5befccd93cb21
Jolan Philippe <jolan.philippe@gmail.com>
2020-11-17 09:55:34 +0100

    C2R rules with erros

--- class2relational/src/main/scala/org/atlanmod/Class2Relational.scala
+++ class2relational/src/main/scala/org/atlanmod/Class2Relational.scala
@@ -1,133 +1,62 @@
 package org.atlanmod
 
-import org.atlanmod.io.{ELink, EMFMetamodel, EMFModel}
+import org.atlanmod.tl.model.Transformation
 import org.atlanmod.tl.model.impl.{OutputPatternElementImpl, RuleImpl, TransformationImpl}
-import org.atlanmod.tl.model.{Model, OutputPatternElement, Rule, Transformation}
-import org.eclipse.emf.common.notify.{Adapter, Notification}
-import org.eclipse.emf.common.util.{EList, TreeIterator}
-import org.eclipse.emf.ecore.resource.Resource
-import org.eclipse.emf.ecore.{EAttribute, EClass, EObject, EOperation, EReference, EStructuralFeature}
-
-//import org.atlanmod.zoo.
+import org.eclipse.emf.ecore.{EClass, EObject, EPackage, EReference}
+import org.atlanmod.model.{ClassHelper, RelationalHelper}
+import org.atlanmod.ELink
 
 class Class2Relational() {
 
-    def getClassModel(): (EMFMetamodel, EMFModel) =
-        // TODO
-        ???
+    private def transformation() : Transformation[EObject, ELink, EClass, EObject, ELink] = {
+        new TransformationImpl[EObject, ELink, EClass, EObject, ELink](
+            List(
+                new RuleImpl(
+                    name = "Class2Table",
+                    types = List(ClassHelper.getEClass_Class),
+                    from = (m, l) => Some(true), // No guard condition
+                    itExpr = (_, _)  => Some(1), // No iterator,
+                    to =
+//                      elem [AttributeClass] ColumnClass "col"
+//                        (fun i m a => BuildColumn (getAttributeId a) (getAttributeName a))
+                      List(
+                        new OutputPatternElementImpl(
+                            name = "tab",
+                            // TODO : why l is a list ?
+                            elementExpr = (_, _, l) => Some(RelationalHelper.buildTable(
+                                ClassHelper.getClass_Id(l.head), ClassHelper.getClass_name(l.head))),
+                            to = List(
+                                new OutputPatternElementReferenceImpl(
 
-    /*
-     Coq notations:
-        (* Rule *)
-        Notation "'rule' rulename 'from' types 'where' guard 'for' iterator 'to' outputpattern " :=
-        (concreteRule rulename types (Some guard) (Some iterator) outputpattern)
-        (right associativity, at level 60):coqtl.
-
-        (* Rule without guard *)
-        Notation "'rule' rulename 'from' types 'for' iterator 'to' outputpattern " :=
-        (concreteRule rulename types (None) (Some iterator) outputpattern)
-        (right associativity, at level 60):coqtl.
-
-        (* Rule without iterator *)
-        Notation "'rule' rulename 'from' types 'where' guard 'to' outputpattern " :=
-        (concreteRule rulename types (Some guard) (None) outputpattern)
-        (right associativity, at level 60):coqtl.
-
-        (* Rule without guard and iterator *)
-        Notation "'rule' rulename 'from' types 'to' outputpattern " :=
-        (concreteRule rulename types (None) (None) outputpattern)
-        (right associativity, at level 60):coqtl.
-    */
-
-    private def rule_Class2Table(mm : EMFMetamodel): Rule[EObject, ELink, EClass, EObject, ELink] = {
-        """
-            rule  {
-                from
-                c : Class
-                to
-                tab: Table (
-                      id <- c.id,
-                      name <- c.name,
-                      columns <- c.attributes->collect(a | thisModule.resolve(a, 'col'))
+                                )
+                            ) // TODO
+                        )
                     )
-            }
-
-            (* Rule without guard *)
-            Notation "'rule' rulename 'from' types 'for' iterator 'to' outputpattern " :=
-            (concreteRule rulename types (None) (Some iterator) outputpattern)
-            (right associativity, at level 60):coqtl.
-
-            rule "Class2Table"
-            from [ClassClass]
-            to [elem [ClassClass] TableClass "tab"
-                (fun i m c => BuildTable (getClassId c) (getClassName c))
-                [link [ClassClass] TableClass TableColumnsReference
-                  (fun tls i m c t =>
-                    maybeBuildTableColumns t
-                      (maybeResolveAll tls m "col" ColumnClass
-                        (maybeSingletons (getClassAttributesObjects c m))))]]
-            ;
-
-        """
-        val name = "Class2Table"
-        val types: List[EClass] = List(EClass)
-        val from: (Model[EObject, ELink], List[EObject]) => Option[Boolean] = (_, _) => Some(true) // No guard condition
-        val iterator: (Model[EObject, ELink], List[EObject]) => Option[Int] = (_, _)  => Some(0) // No iterator
-
-        //        name: String,
-        //     elementExpr: (Int, Model[SME, SML], List[SME]) => Option[TME],
-        //     outputElemRefs: List[OutputPatternElementReference[SME, SML, TME, TML]]
-
-        val output_tab : OutputPatternElement[EObject, ELink, EObject, ELink] =
-            new OutputPatternElementImpl(
-                name = "tab",
-                elementExpr = (i, m, l) => None, // TODO
-                outputElemRefs = List() // TODO
+                ),
+                new RuleImpl(
+                    name = "Attribute2Column",
+                    types = List(ClassHelper.getEClass_Attribute),
+                    from = (_, l) => ClassHelper.getAttribute_isDerived(l.head),
+                    itExpr = (_, _) => Some(1),
+                    to =
+//                      elem [AttributeClass] ColumnClass "col"
+//                        (fun i m a => BuildColumn (getAttributeId a) (getAttributeName a))
+                      List(
+                        new OutputPatternElementImpl(
+                            name = "col",
+                            // TODO : why l is a list ?
+                            // ConcreteSyntax.v : elem
+                            //
+                            elementExpr = (_, _, l) => Some(RelationalHelper.buildColumn(
+                                ClassHelper.getAttribute_Id(l.head), ClassHelper.getAttribute_name(l.head))
+                            ),
+                            to = List() // TODO
+                        )
+                    )
+                )
             )
-
-        val to: List[OutputPatternElement[EObject, ELink, EObject, ELink]] = List(output_tab)
-        new RuleImpl(name, types, from, iterator, to)
+        )
     }
 
-    private def rule_Attribute2Column(): Rule[EObject, ELink, EClass, EObject, ELink] = {
-        """
-            rule Attribute2Column {
-                from
-                a : Attribute (not a.derived)
-                to
-                col: Column (
-                      id <- a.id,
-                      name <- a.name,
-                      reference <- thisModule.resolve(a.type, 'tab')
-                    )
-            }
-
-            rule "Attribute2Column"
-            from [AttributeClass]
-            where (fun m a => negb (getAttributeDerived a))
-            to [elem [AttributeClass] ColumnClass "col"
-                (fun i m a => BuildColumn (getAttributeId a) (getAttributeName a))
-                [link [AttributeClass] ColumnClass ColumnReferenceReference
-                  (fun tls i m a c =>
-                    maybeBuildColumnReference c
-                      (maybeResolve tls m "tab" TableClass
-                        (maybeSingleton (getAttributeTypeObject a m))))]]
-        """
-        val name = "Attribute2Column"
-        val types: List[EClass] = List(EClass)
-        val from: (Model[EObject, ELink], List[EObject]) => Option[Boolean] = (m, l) => Some(true)
-        val iterator: (Model[EObject, ELink], List[EObject]) => Option[Int] = (m, l) => Some(0)
-        val to: List[OutputPatternElement[EObject, ELink, EObject, ELink]] = List()
-        new RuleImpl(name, types, from, iterator, to)
-    }
-
-
-    private def getRules(): List[Rule[EObject, ELink, EClass, EObject, ELink]] = {
-        List(rule_Class2Table(), rule_Attribute2Column())
-    }
-
-    private def getTransformation(): Transformation[EObject, ELink, EClass, EObject, ELink] = {
-        new TransformationImpl(getRules())
-    }
 
 }
