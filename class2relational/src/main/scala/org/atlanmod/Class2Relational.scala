ac1057b16d6d4272fc85c1c64ac9d6cecb5192ed
Jolan Philippe <jolan.philippe@gmail.com>
2020-11-18 15:26:10 +0100

    generated Java classes

--- class2relational/src/main/scala/org/atlanmod/Class2Relational.scala
+++ class2relational/src/main/scala/org/atlanmod/Class2Relational.scala
@@ -1,6 +1,7 @@
 package org.atlanmod
 
 import org.atlanmod.model.{ClassHelper, RelationalHelper}
+import org.atlanmod.relationalModel.impl.{ColumnImpl, TableImpl}
 import org.atlanmod.tl.model.Transformation
 import org.atlanmod.tl.model.impl.{OutputPatternElementImpl, OutputPatternElementReferenceImpl, RuleImpl, TransformationImpl}
 import org.eclipse.emf.ecore.{EClass, EObject}
@@ -8,22 +9,28 @@ import org.eclipse.emf.ecore.{EClass, EObject}
 class Class2Relational() {
 
     private def transformation() : Transformation[EObject, ELink, EClass, EObject, ELink] = {
+
         new TransformationImpl[EObject, ELink, EClass, EObject, ELink](
             List(
                 new RuleImpl(
                     name = "Class2Table",
-                    types = List(ClassHelper.getEClass_Class),
+                  // CoqTL : use a label instead of an EClass :
+                    // types = List(ClassHelper.CLASS)
+                    types = List(classModel.Class),
                     from = (_, _) => Some(true), // No guard condition
-                    itExpr = (_, _)  => Some(1), // No iterator,
+                    itExpr = (_, _)  => Some(1), // No iterator
                     to =
-//                      elem [AttributeClass] ColumnClass "col"
-//                        (fun i m a => BuildColumn (getAttributeId a) (getAttributeName a))
                       List(
                         new OutputPatternElementImpl(
                             name = "tab",
                             // TODO : why l is a list ? How to use ``elem''
-                            elementExpr = (_, _, l) => Some(RelationalHelper.buildTable(
-                                ClassHelper.getClass_Id(l.head), ClassHelper.getClass_name(l.head))),
+                            elementExpr = (_, _, l) => {
+                                val _class = l.head.asInstanceOf[classModel.Class]
+                                val table = new TableImpl
+                                table.setId(_class.getId)
+                                table.setName(_class.getName)
+                                Some(table)
+                            },
                             outputElemRefs = List(
                                 new OutputPatternElementReferenceImpl(null) // TODO
                             )
@@ -32,9 +39,9 @@ class Class2Relational() {
                 ),
                 new RuleImpl(
                     name = "Attribute2Column",
-                    types = List(ClassHelper.getEClass_Attribute),
-                    from = (_, l) => Some(ClassHelper.getAttribute_isDerived(l.head)), // TODO probably wrong
-                    itExpr = (_, _) => Some(1),
+                    types = List(classModel.Attribute),
+                    from = (_, l) => Some(true), // TODO : not derived
+                    itExpr = (_, _) => Some(1), // No where clause
                     to =
 //                      elem [AttributeClass] ColumnClass "col"
 //                        (fun i m a => BuildColumn (getAttributeId a) (getAttributeName a))
@@ -44,9 +51,14 @@ class Class2Relational() {
                             // TODO : why l is a list ? How to use ``elem''
                             // ConcreteSyntax.v : elem
                             //
-                            elementExpr = (_, _, l) => Some(RelationalHelper.buildColumn(
-                                ClassHelper.getAttribute_Id(l.head), ClassHelper.getAttribute_name(l.head))
-                            ),
+                            elementExpr = (_, _, l) =>
+                                {
+                                    val attribute = l.head.asInstanceOf[classModel.Attribute]
+                                    val column = new ColumnImpl
+                                    column.setId(attribute.getId)
+                                    column.setName(attribute.getName)
+                                    Some(column)
+                                },
                             outputElemRefs = List(
                                 new OutputPatternElementReferenceImpl(null) // TODO
                             )
