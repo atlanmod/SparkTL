098ced8ccb05a41cbfc5ca3c5cd72e4fcfedbbd3
Jolan Philippe <jolan.philippe@gmail.com>
2021-04-20 17:16:07 +0200

    Fixed issue in the parallel tuple generation

--- transformation/src/test/scala/org/atlanmod/tl/util/TestTupleUtils.scala
+++ transformation/src/test/scala/org/atlanmod/tl/util/TestTupleUtils.scala
@@ -11,9 +11,11 @@ test("tuples_up_to_n"){
     val max_arity = 3
     val res1 = TupleUtils.tuples_up_to_n_prime_parallel(rdd, max_arity)
     val res2 = TupleUtils.tuples_up_to_n_prime(list, max_arity)
+    val res3 = TupleUtils.tuples_up_to_n_super(list, max_arity, sc)
     val expected = TupleUtils.tuples_up_to_n(list, max_arity)
     assert(ListUtils.eqList(res2, expected))
     assert(ListUtils.eqList(res1, expected))
+    assert(ListUtils.eqList(res3, expected))
 }
 
 }
