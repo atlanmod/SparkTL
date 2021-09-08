fccada0ce4315f62f50a41f2d060508cb87b4a3a
Jolan Philippe <jolan.philippe@gmail.com>
2021-09-08 16:43:49 +0200

    Loader for elements of Imdb case

--- sparktl-transformations/src/main/scala/org/atlanmod/findcouples/model/movie/MovieLoader.scala
+++ sparktl-transformations/src/main/scala/org/atlanmod/findcouples/model/movie/MovieLoader.scala
@@ -1,14 +1,39 @@
 package org.atlanmod.findcouples.model.movie
 
-import java.io.File
+import java.io.{ByteArrayOutputStream, File, ObjectOutputStream}
+import java.security.MessageDigest
 
-import org.eclipse.emf.common.util.URI
-import org.eclipse.emf.ecore.EPackage
+import javax.xml.bind.DatatypeConverter
+import org.atlanmod.findcouples.model.movie.element.{MovieActor, MovieActress, MovieMovie, MovieType}
+import org.eclipse.emf.common.util.{EList, URI}
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl
 import org.eclipse.emf.ecore.resource.{Resource, ResourceSet}
 import org.eclipse.emf.ecore.xmi.impl.{EcoreResourceFactoryImpl, XMIResourceFactoryImpl}
+import org.eclipse.emf.ecore.{EAttribute, EClass, EObject, EPackage}
+
+import scala.collection.mutable
 
 object MovieLoader{
+
+    object Checksum {
+        def getMD5(o: Any): String = {
+            var baos: ByteArrayOutputStream = null
+            var oos: ObjectOutputStream = null
+            try {
+                baos = new ByteArrayOutputStream()
+                oos = new ObjectOutputStream(baos)
+                oos.writeObject(o)
+
+                val md : MessageDigest = MessageDigest.getInstance("MD5")
+                val digest: Array[Byte] = md.digest(baos.toByteArray)
+                DatatypeConverter.printHexBinary(digest)
+
+            } finally {
+                oos.close()
+                baos.close()
+            }
+        }
+    }
 
     private var resSet : ResourceSet = null
 
@@ -21,25 +46,69 @@ object MovieLoader{
         resSet = new ResourceSetImpl
     }
 
+    def key(obj : Object): String = {
+        Checksum.getMD5(obj)
+    }
+
     def load(xmi_file: String, ecore_file: String) : MovieModel = {
+        // Instantiate global ResourceSet
         instantiateRS()
 
-        val mm_absolutePath = new File(ecore_file).getAbsolutePath
-        val mm_uri = URI.createURI(mm_absolutePath)
-        val resource_ecore: Resource = resSet.getResource(mm_uri, true)
+        // Register the ecore file (containing the metamodel)
+        val absolutePath_metamodel = new File(ecore_file).getAbsolutePath
+        val uri_metamodel = URI.createURI(absolutePath_metamodel)
+        val resource_ecore: Resource = resSet.getResource(uri_metamodel, true)
         val pack : EPackage = resource_ecore.getContents.get(0).asInstanceOf[EPackage]
         val packageRegistry = resSet.getPackageRegistry()
         packageRegistry.put(pack.getNsURI, pack)
 
-        val xmi_absolutePath = new File(xmi_file).getAbsolutePath
-        val xmi_uri = URI.createURI(xmi_absolutePath)
-        val resource_xmi : Resource = resSet.getResource(xmi_uri, true)
+        // Register the xmi file (containing the model)
+        val absolutePath_model = new File(xmi_file).getAbsolutePath
+        val uri_model = URI.createURI(absolutePath_model)
+        val resource : Resource = resSet.getResource(uri_model, true)
 
-        val res =resource_xmi.getContents.get(0)
-//        print(res)
+        var elements: List[MovieElement] = List()
+        var links: List[MovieLink] = List()
+        var map: mutable.Map[String, MovieElement] = new mutable.HashMap[String, MovieElement]()
 
-        val elements: List[MovieElement] = List()
-        val links: List[MovieLink] = List()
+        for (i <- 0 until resource.getContents.size()){
+//        for (i <- 0 until 2){
+            val obj: EObject = resource.getContents.get(i)
+            val cl: EClass = obj.eClass()
+            val attributes: EList[EAttribute] = cl.getEAllAttributes
+            obj.eClass().getName match {
+                case MovieMetamodel.MOVIE =>
+                    val title = obj.eGet(attributes.get(0)).asInstanceOf[String]
+                    val rating = obj.eGet(attributes.get(1)).asInstanceOf[Double]
+                    val year = obj.eGet(attributes.get(2)).asInstanceOf[Int]
+                    val type_ = MovieType.stringToMovieTypeVal(obj.eGet(attributes.get(3)).asInstanceOf[String])
+                    val movie = new MovieMovie(title, rating, year, type_)
+                    elements = movie :: elements
+                    map.put(key(movie), movie)
+                case MovieMetamodel.ACTOR =>
+                    val name = obj.eGet(attributes.get(0)).asInstanceOf[String]
+                    val actor = new MovieActor(name)
+                    elements = actor :: elements
+                    map.put(key(actor), actor)
+                case MovieMetamodel.ACTRESS =>
+                    val name = obj.eGet(attributes.get(0)).asInstanceOf[String]
+                    val actress = new MovieActress(name)
+                    elements = actress :: elements
+                    map.put(key(actress), actress)
+            }
+        }
+
+        for (i <- 0 until resource.getContents.size()){
+            resource.getContents.get(i).eClass().getName match {
+                case MovieMetamodel.MOVIE => //TODO build the MovieToPersons
+                case MovieMetamodel.ACTOR => //TODO build the PersonToMovies
+                case MovieMetamodel.ACTRESS => //TODO build the PersonToMovies
+                case MovieMetamodel.COUPLE => //TODO build the Couple, the CoupleToP1 and CoupleToP2
+                case MovieMetamodel.CLIQUE => //TODO build the Clique, the CliqueToPersons
+            }
+        }
+        println(elements.size)
+        println(links.size)
         new MovieModel(elements, links)
     }
 
--- sparktl-transformations/src/test/scala/org/atlanmod/findcouples/transformation/TestLoader.scala
+++ sparktl-transformations/src/test/scala/org/atlanmod/findcouples/transformation/TestLoader.scala
@@ -8,7 +8,7 @@ class TestLoader extends AnyFunSuite {
     val path = "/home/jolan/Scala/SparkTL/SparkTL/sparktl-transformations/src/main/resources/imdb_models/"
 
     test("test loader"){
-        val filepath = path+"imdb-0.2.xmi"
+        val filepath = path+"imdb-1.0.xmi"
         val ecore = path+"movies.ecore"
         MovieLoader.load(filepath, ecore)
     }
